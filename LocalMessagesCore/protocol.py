60261ebf4969b8bb886419b6b1c81313641b8e62
Lyang1273 <2609350581@qq.com>
2026-07-22 15:11:31 +0800

    更改登录注册接口url

--- LocalMessagesCore/protocol.py
+++ LocalMessagesCore/protocol.py
@@ -3,8 +3,8 @@
 PROTOCOL_VERSION = 1
 
 CONNECT_PATH = "/api/connect"
-SIGN_UP_PATH = "/api/register"
-SIGN_IN_PATH = "/api/login"
+SIGN_UP_PATH = "/api/sign_up"
+SIGN_IN_PATH = "/api/sign_in"
 LOGOUT_PATH = "/api/logout"
 MESSAGES_PATH = "/api/messages"
 EVENTS_PATH = "/api/events"
